b5be9fad6a9bed8183f3824746f2c601b23adc48
Kapil Ahuja <Kapil1.ahuja@ril.com>
2024-03-08 19:34:53 +0530

    spring security

--- src/main/java/net/javaguides/ems/controller/EmployeeController.java
+++ src/main/java/net/javaguides/ems/controller/EmployeeController.java
@@ -5,17 +5,19 @@ import net.javaguides.ems.dto.EmployeeDto;
 import net.javaguides.ems.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("/api/employees")
+@RequestMapping("/api/v1/employees")
 public class EmployeeController {
 
     private EmployeeService employeeService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
         EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
